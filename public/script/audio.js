3a6f9c53e6ae5e90922bd900b084cd2a57b9e4b7
A-pilip <areoayomide2008@gmail.com>
2024-12-15 08:24:01 +0100

    updating audio.js

--- public/script/audio.js
+++ public/script/audio.js
@@ -1,1 +1,17 @@
-const randomNumber = Math.floor(Math.random() * 3);
+const availableSong = ["mood.mp3", "Duffel-Bag.mp3", "Stronger.mp3"];
+
+const randomNumber = Math.floor(Math.random() * availableSong.length);
+
+console.log(randomNumber);
+
+console.log(`./audio/${availableSong[randomNumber]}`);
+
+var song = new Audio(`./public/audio/${availableSong[randomNumber]}`);
+song.autoplay = true;
+window.addEventListener("click", function (events) {
+  if (song.paused) {
+    song.play();
+  } else {
+    console.log("Song is playing");
+  }
+});
